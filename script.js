ae9b388caf62c46af79229ef8127a32483de3c00
Klesk <klesk@whatever.com>
2017-09-14 19:38:17 +0300

    Update

---
+++ script.js
@@ -0,0 +1,23 @@
+﻿jQuery(document).ready(function () {
+    jQuery('.tab-links a').on('click', function (e) {
+        var currentAttrValue = jQuery(this).attr('href');
+
+        // Show/Hide Tabs
+        jQuery('.tab-wrapper ' + currentAttrValue).show().siblings().hide();
+
+        // Change/remove current tab to active
+        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+
+        e.preventDefault();
+    });
+
+    jQuery(".top-menu-icon-wrapper").click(function () {
+        jQuery(".top-nav-list").toggleClass("top-nav-list-responsive");
+    });
+
+    jQuery(".interior-header, .exterior-header, .safety-header, .other-header, .warr-header").click(function () {
+        var specTarget = this.className.replace("-header", "");
+        jQuery(".spec-" + specTarget).fadeToggle();
+        jQuery(this).children("div.minus-button").toggleClass("plus-button");
+    });
+});
